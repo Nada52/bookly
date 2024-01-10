a142ef09c8f4f857b44985d531875373216b80ff
nada elborhamy <73416301+Nada52@users.noreply.github.com>
2024-01-10 03:59:29 +0200

    create cutom book details app bar

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,11 @@
+import 'package:bookly/features/home/presentation/views/book_datails_view.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
+  static const kBookDetailsView = '/bookDetailsView';
   static final GoRouter router = GoRouter(
     routes: <RouteBase>[
       GoRoute(
@@ -24,6 +26,12 @@ abstract class AppRouter {
         path: kHomeView,
         builder: (context, state) {
           return const HomeView();
+        },
+      ),
+      GoRoute(
+        path: kBookDetailsView,
+        builder: (context, state) {
+          return const BookDetailsView();
         },
       ),
     ],
